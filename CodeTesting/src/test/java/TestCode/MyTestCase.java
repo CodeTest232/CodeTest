74630621f85c7d391988db86b987c2b5062ec38f
92349 <92349@DESKTOP-9MCKVCH>
2020-06-01 01:42:02 +0500

    Successfully

---
+++ CodeTesting/src/test/java/TestCode/MyTestCase.java
@@ -0,0 +1,35 @@
+package TestCode;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class MyTestCase extends TestCase {
+	 
+    protected ListTest lstTest = new ListTest();
+ 
+    protected void setUp() {
+        lstTest.add("Apple");
+        lstTest.add("Orange");
+        lstTest.add("Grapes");
+    }
+ 
+    public void testSize() {
+        assertEquals("Checking size of List", 3, lstTest.size());
+    }
+ 
+    public void testAdd() {
+        lstTest.add("Banana");
+        assertEquals("Adding 1 more fruit to list", 4, lstTest.size());
+    }
+ 
+    public void testRemove() {
+        lstTest.remove("Orange");
+        assertEquals("Removing 1 fruit from list", 2, lstTest.size());
+    }
+ 
+    protected void tearDown() {
+        lstTest.removeAll();
+    }
+}
---
+++ CodeTesting/src/test/java/TestCode/TestCases.java
@@ -0,0 +1,24 @@
+package TestCode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCases {
+
+	   @Test
+	    public void firstTest() {
+	        System.out.println("First Test");
+	    }
+	     
+	    @Test
+	    public void thirdTest() {
+	        System.out.println("Third Test");
+	    }
+	     
+	    @Test
+	    public void secondTest() {
+	        System.out.println("Second Test");
+	    }
+
+}
